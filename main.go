6c6f5940a6f01aaae55fe2e359bb5638a8dfc6a4
heronh <heronhurpia@gmail.com>
2024-05-31 12:09:06 -0300

    Criada função server

--- main.go
+++ main.go
@@ -5,20 +5,23 @@ import (
 	"net/http"
 )
 
+func server(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, `<!DOCTYPE html>
+	<html>
+	<head>
+		 <title>Simple Go Web Page</title>
+	</head>
+	<body>
+		 <h1>Hello from Go!</h1>
+		 <p>This is a basic web page served by a Go server.</p>
+	</body>
+	</html>`)
+}
+
 func main() {
 	// Create a handler function
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, `<!DOCTYPE html>
-<html>
-<head>
-    <title>Simple Go Web Page</title>
-</head>
-<body>
-    <h1>Hello from Go!</h1>
-    <p>This is a basic web page served by a Go server.</p>
-</body>
-</html>`)
-	})
+	http.HandleFunc("/", server)
 
 	// Start the server
 	fmt.Println("Server listening on port 8080...")
