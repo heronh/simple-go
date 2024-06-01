f8c9b311bac1e0a03ffea2db787c46f34d5d49c5
heronh <heronhurpia@gmail.com>
2024-06-01 14:08:24 -0300

    Criada estrutura de diretórios de postgres

---
+++ postgres/app/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"com.github/teste-package/server"
+	"github.com/gin-gonic/gin"
+)
+
+/*
+ *		Cria um servidor que só tem um path implementado, /
+ */
+func main() {
+
+	// Create a Gin router
+	router := gin.Default()
+
+	// Load HTML templates
+	router.LoadHTMLGlob("templates/*")
+
+	// Define a route for the root path ("/")
+	router.GET("/", server.Server)
+
+	// Serve static files (CSS) from the 'static' directory
+	router.Static("/static", "./static")
+
+	// Start the server
+	router.Run(":8080")
+}
