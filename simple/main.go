70c52895d12a498b9bf70e22974d5d2c5ae221fa
heronh <heronhurpia@gmail.com>
2024-06-01 11:59:18 -0300

    Finalizado programa \"simples\"

--- simple/main.go
+++ simple/main.go
@@ -2,46 +2,39 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 )
 
-func server(c *gin.Context) {
+/*
+**		Cria um servidor que só tem um path implementado, /
+ */
+func main() {
 
+	// Create a Gin router
+	router := gin.Default()
+
+	// Load HTML templates
+	router.LoadHTMLGlob("templates/*")
+
+	// Define a route for the root path ("/")
+	router.GET("/", server)
+
+	// Serve static files (CSS) from the 'static' directory
+	router.Static("/static", "./static")
+
+	// Start the server
+	router.Run(":8080")
+}
+
+/*
+**		Servidor que responde com a página index.html e
+** envia um parâmetro, title.
+ */
+func server(c *gin.Context) {
 	fmt.Println("server")
 	c.HTML(http.StatusOK, "index.html", gin.H{
 		"title": "hello",
 	})
-}
-
-func main() {
-
-	absPath, _ := filepath.Abs("templates/**/*")
-	log.Println(absPath)
-
-	// // Create a Gin router
-	router := gin.Default()
-
-	// // Load HTML templates (assuming "index.html" is in the "templates" directory)
-	router.LoadHTMLGlob("templates/*")
-
-	// // Define a route for the root path ("/")
-	router.GET("/", server)
-
-	// // Serve static files (CSS) from the 'static' directory
-	router.Static("/static", "./static")
-
-	// // Define a route for "/ping"
-	// router.GET("/ping", func(c *gin.Context) {
-	// 	c.JSON(http.StatusOK, gin.H{
-	// 		"message": "pong",
-	// 	})
-	// })
-
-	// // Start the server
-	router.Run(":8080")
-
 }
