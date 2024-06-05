8ed24d627150525f5600031b25be0c932392ee2c
heronh <heronhurpia@gmail.com>
2024-06-05 17:59:59 -0300

    Implementando exibir lista em html

--- postgres/server/pgsql.go
+++ postgres/server/pgsql.go
@@ -19,6 +19,12 @@ const (
 	dbname   = "portal"
 )
 
+type users struct {
+	id    int
+	name  string
+	email string
+}
+
 /*
  *		Servidor que responde com a página index.html e
  * envia um parâmetro, title.
@@ -26,23 +32,57 @@ const (
 func Pgsql(c *gin.Context) {
 	fmt.Println("postgres")
 	connection := connect()
-	fmt.Printf("%s\n", connection)
+
+	query := "select id, name, email from users order by id"
+	fmt.Printf("%s\n", query)
+	list := exec_query(query)
+	for _, user := range list {
+		fmt.Printf("%02d - %30s %s\n", user.id, user.name, user.email)
+	}
 
 	c.HTML(http.StatusOK, "postgres.html", gin.H{
 		"title":      "Postgres",
 		"connection": connection,
+		"users":      list,
 	})
 
 }
 
+func exec_query(query string) []users {
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	//fmt.Printf("query: %s\n", psqlInfo)
+
+	// Abre conexão com db
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Busca lista de usuários
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var elements []users
+	for rows.Next() {
+		var element users
+		err := rows.Scan(&element.id, &element.name, &element.email) // match these with the struct fields
+		if err != nil {
+			log.Fatal(err)
+		}
+		elements = append(elements, element)
+	}
+	return elements
+}
+
+// Executa conexão com DB
 func connect() string {
-
-	fmt.Println("Tentativa de conexão")
-
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-	fmt.Printf("query: %s\n", psqlInfo)
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	//fmt.Printf("query: %s\n", psqlInfo)
 
 	// Abre conexão com db
 	db, err := sql.Open("postgres", psqlInfo)
