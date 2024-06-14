0568b9a91771566cee1ae2c2b40c79fc02a14d79
heronh <heronhurpia@gmail.com>
2024-06-14 08:14:49 -0300

    Iniciada migração

--- postgres/server/migrate.go
+++ postgres/server/migrate.go
@@ -1,44 +1,40 @@
 package server
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-migrate/migrate"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
 )
 
-type Migration_t struct {
-	Exists   string
-	Create   string
-	Populate string
-}
-
-var Migrations []Migration_t
-
-func init() {
-
-	Migrations = []Migration_t{
-		{"a", "b", "c"},
-		{"a", "b", "c"},
-		{"a", "b", "c"},
-	}
-}
-
 func Migrate(c *gin.Context) {
-	fmt.Println("postgres")
-	//connection := connect()
 
-	for _, m := range Migrations {
-		fmt.Printf("Name: %s %s, City: %s\n", m.Exists, m.Create, m.Populate)
+	fmt.Println("Migrate")
+
+	db, err := sql.Open("postgres", "postgres://user:password@localhost:5432/your_database?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
 	}
-}
 
-/*
-var create_users := "CREATE TABLE users (
-   user_id SERIAL PRIMARY KEY,
-   name VARCHAR(50) UNIQUE NOT NULL,
-   email VARCHAR(255) UNIQUE NOT NULL,
-   password VARCHAR(255) NOT NULL,
-   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);"
-*/
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"migrations.sql", // Migration file location
+		"postgres", driver)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := m.Up(); err != nil { // Apply all pending migrations
+		log.Fatal(err)
+	}
+	fmt.Println("Migrations successful!")
+}
---
+++ postgres/server/migrations.sql
@@ -0,0 +1,12 @@
+-- Migration 0001_create_users_table.up.sql
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Migration 0001_create_users_table.down.sql 
+DROP TABLE users;
