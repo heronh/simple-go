73a417b8dc488b13bd434aa594ffd6d6694ae2c8
heronh <heronhurpia@gmail.com>
2024-06-18 07:44:10 -0300

    Conexão com rede de containers funcionando

--- postgres/server/migrate.go
+++ postgres/server/migrate.go
@@ -7,9 +7,9 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/golang-migrate/migrate"
-	"github.com/golang-migrate/migrate/database/postgres"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
+	//"github.com/golang-migrate/migrate"
+	//"github.com/golang-migrate/migrate/database/postgres"
+	//_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
 )
 
@@ -18,42 +18,20 @@ func Migrate(c *gin.Context) {
 	fmt.Println("Migrate")
 
 	//db, err := sql.Open("postgres", "postgres://user:password@localhost:5432/your_database?sslmode=disable")
-	db, err := sql.Open("postgres", "postgres://sat:U3t8QYg@192.168.30.5:5432/portal?sslmode=disable")
+	db, err := sql.Open("postgres", "postgres://postgres:strong_password@172.18.0.2:5432/postgres?sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
 	coneccao := "Banco de dados connectado com sucesso"
 	fmt.Println(coneccao)
 
-	driver, err := postgres.WithInstance(db, &postgres.Config{})
-	if err != nil {
-		log.Fatal(err)
-	}
-	drv := "Driver carregado"
-	fmt.Println(drv)
-
-	m, err := migrate.NewWithDatabaseInstance(
-		"migrations.sql", // Migration file location
-		"postgres", driver)
-	if err != nil {
-		log.Fatal(err)
-	}
-	file := "Arquivo com as migrações"
-	fmt.Println(file)
-
-	if err := m.Up(); err != nil { // Apply all pending migrations
-		log.Fatal(err)
-	}
-	result := "Migrations successful!"
-	fmt.Println(result)
+	err = db.Ping()
+    if err != nil {
+        log.Fatal("Error pinging database:", err)
+    }
+    fmt.Println("Successfully connected to PostgreSQL database!")	
 
 	c.HTML(http.StatusOK, "migrations.html", gin.H{
 		"coneccao": coneccao,
-		"driver":   drv,
-		"file":     file,
-		"result":   result,
 	})
-
-	/*
-	 */
 }
