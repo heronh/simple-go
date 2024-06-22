54f658adce7c1a5a292701398bf27989a3a7fddb
heronh <heronhurpia@gmail.com>
2024-06-22 16:05:03 -0300

    Alterada estrutura das migrações

--- templates/server/migrate.go
+++ templates/server/migrate.go
@@ -15,60 +15,57 @@ import (
 func Migrate(c *gin.Context) {
 
 	mensagens := []string{"Iniciando migrações..."}
+	defer func() {
+		c.HTML(http.StatusOK, "migrations.html", gin.H{
+			"Title":          "Migrações",
+			"Heading":        "Teste do comunicação!",
+			"Message":        "",
+			"migrate_active": "h5",
+			"Mensagens":      mensagens,
+		})
+	}()
 
-	runOnce := true
-	for runOnce {
-		psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-		mensagens = append(mensagens, psqlInfo)
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	mensagens = append(mensagens, psqlInfo)
 
-		// Abre conexão com db
-		db, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=loterias password=mysecretpassword sslmode=disable")
-		if err != nil {
-			mensagens = append(mensagens, "failed to connect database")
-			mensagens = append(mensagens, err.Error())
-			break
-		}
-		mensagens = append(mensagens, "Banco de dados conectado!")
-		defer db.Close()
+	// Abre conexão com db
+	db, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=loterias password=mysecretpassword sslmode=disable")
+	if err != nil {
+		mensagens = append(mensagens, "failed to connect database")
+		mensagens = append(mensagens, err.Error())
+		return
+	}
+	mensagens = append(mensagens, "Banco de dados conectado!")
+	defer db.Close()
 
-		// Initialize the driver for PostgreSQL
-		driver, _ := postgres.WithInstance(db.DB(), &postgres.Config{})
-		m, err := migrate.NewWithDatabaseInstance(
-			"file:///Users/heronhurpia/Sites/simple-go/templates/migrations",
-			"postgres", driver)
-		if err != nil {
-			mensagens = append(mensagens, "Falha ao executar migração")
-			mensagens = append(mensagens, err.Error())
-			break
-		}
-
-		// if err := m.Down(); err != nil {
-		// 	if err == migrate.ErrNoChange {
-		// 		fmt.Println("No migration changes were applied")
-		// 		return
-		// 	}
-		// 	panic(err)
-		// }
-
-		// Migrate all the way up
-		if err := m.Up(); err != nil {
-			if err == migrate.ErrNoChange {
-				mensagens = append(mensagens, "No migration changes were applied")
-				break
-			}
-			mensagens = append(mensagens, "Falha ao aplicar migração")
-			mensagens = append(mensagens, err.Error())
-			break
-		}
-
-		runOnce = false
+	// Initialize the driver for PostgreSQL
+	driver, _ := postgres.WithInstance(db.DB(), &postgres.Config{})
+	m, err := migrate.NewWithDatabaseInstance(
+		"file:///Users/heronhurpia/Sites/simple-go/templates/migrations",
+		"postgres", driver)
+	if err != nil {
+		mensagens = append(mensagens, "Falha ao executar migração")
+		mensagens = append(mensagens, err.Error())
+		return
 	}
 
-	c.HTML(http.StatusOK, "migrations.html", gin.H{
-		"Title":          "Migrações",
-		"Heading":        "Teste do comunicação!",
-		"Message":        "",
-		"migrate_active": "h5",
-		"Mensagens":      mensagens,
-	})
+	// if err := m.Down(); err != nil {
+	// 	if err == migrate.ErrNoChange {
+	// 		fmt.Println("No migration changes were applied")
+	// 		return
+	// 	}
+	// 	panic(err)
+	// }
+
+	// Migrate all the way up
+	if err := m.Up(); err != nil {
+		if err == migrate.ErrNoChange {
+			mensagens = append(mensagens, "No migration changes were applied")
+			return
+		}
+		mensagens = append(mensagens, "Falha ao aplicar migração")
+		mensagens = append(mensagens, err.Error())
+		return
+	}
+	mensagens = append(mensagens, "Migração executada com sucesso!")
 }
