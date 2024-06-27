4fa39bc0a5031d385ebf7a00f83972e747092fbc
heronh <heronhurpia@gmail.com>
2024-06-27 08:05:38 -0300

    Testes com autenticação

--- auth/main.go
+++ auth/main.go
@@ -12,8 +12,33 @@ import (
  */
 func main() {
 
+	// Simulate some private data
+	var secrets = gin.H{
+		"foo":    gin.H{"email": "foo@bar.com", "phone": "123433"},
+		"austin": gin.H{"email": "austin@example.com", "phone": "666"},
+		"lena":   gin.H{"email": "lena@guapa.com", "phone": "523443"},
+	}
+
 	// Create a Gin router
 	router := gin.Default()
+	// gin.Accounts is a shortcut for map[string]string
+	authorized := router.Group("/admin", gin.BasicAuth(gin.Accounts{
+		"foo":    "bar",    // user:foo password:bar
+		"austin": "1234",   // user:austin password:1234
+		"lena":   "hello2", // user:lena password:hello2
+		"manu":   "4321",   // user:manu password:4321
+	}))
+
+	// hit "localhost:8080/admin/secrets
+	authorized.GET("/secrets", func(c *gin.Context) {
+		// Get user, it was set by the BasicAuth middleware
+		user := c.MustGet(gin.AuthUserKey).(string)
+		if secret, ok := secrets[user]; ok {
+			c.JSON(http.StatusOK, gin.H{"user": user, "secret": secret})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
+		}
+	})
 
 	// Load HTML templates
 	router.LoadHTMLGlob("templates/*.html")
@@ -22,6 +47,7 @@ func main() {
 	router.Static("/static", "./static")
 
 	// Define a route for the root path ("/")
+	router.Use(auth.Required())
 	router.GET("/", server.Home)
 	router.GET("/migrate", server.Migrate)
 	router.GET("/todo", server.Todo)
